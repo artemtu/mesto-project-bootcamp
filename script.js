a029ad355c0058ca1141abde51bf07b988eeada4
Artem Tulupov <artemtu94@gmail.com>
2023-05-22 17:33:20 +0400

    add scriptjs cards & bugfix size of cards

--- script.js
+++ script.js
@@ -39,3 +39,49 @@ function handleFormSubmit(evt) {
 
 activeAndClosePopup();
 formElement.addEventListener("submit", handleFormSubmit);
+
+const elementSection = document.querySelector(".element");
+const elementImage = elementSection.querySelector(".element__image");
+const elementTitle = elementSection.querySelector(".element__title");
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const elementSections = document.querySelectorAll(".element");
+
+initialCards.forEach((card, index) => {
+  const elementSection = elementSections[index];
+  const elementImage = elementSection.querySelector(".element__image");
+  const elementTitle = elementSection.querySelector(".element__title");
+  elementTitle.textContent = card.name;
+  elementImage.src = card.link;
+  elementImage.alt = card.name;
+});
+
+
+
+
